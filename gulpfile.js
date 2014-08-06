e35a387cbcd52dac54ea9382127c81dfde9d63b4
Matt Traynham <mrt6467@alum.uncw.edu>
2014-08-05 22:35:00 -0400

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+var gulp = require('gulp'),
+    browserify = require('browserify'),
+    bump = require('gulp-bump'),
+    coffeeify = require('coffeeify'),
+    coffeeLint = require('gulp-coffeelint'),
+    rename = require('gulp-rename'),
+    source = require('vinyl-source-stream'),
+    streamify = require('gulp-streamify'),
+    uglify = require('gulp-uglify'),
+    watchify = require('watchify');
+
+gulp.task('lint', function () {
+    return gulp.src('src/*.coffee')
+        .pipe(coffeeLint('.coffeelint.json'))
+        .pipe(coffeeLint.reporter())
+        .pipe(coffeeLint.reporter('fail'));
+});
+
+gulp.task('build', function () {
+    var bundler = browserify({
+        basedir: __dirname,
+        entries: ['./src/index.coffee'],
+        extensions: ['.coffee'],
+        debug: global.isProduction ? true : false,
+        cache: {},
+        packageCache: {},
+        fullPaths: false
+    }).transform(coffeeify);
+
+    var bundle = function () {
+        return bundler
+            .bundle()
+            .pipe(source('matrix.js'))
+            .pipe(gulp.dest('./'))
+            .pipe(streamify(uglify()))
+            .pipe(rename('matrix.min.js'))
+            .pipe(gulp.dest('./'));
+    };
+    if (global.isWatching) {
+        bundler = watchify(bundler);
+        bundler.on('update', bundle);
+    }
+    return bundle();
+});
+
+gulp.task('setWatch', function () {
+    global.isWatching = true;
+});
+
+gulp.task('setProduction', function () {
+    global.isProduction = true;
+});
+
+var bumpFn = function (type) {
+    gulp.src(['./bower.json', './package.json'])
+        .pipe(bump({type: type}))
+        .pipe(gulp.dest('./'));
+};
+
+// Default Task
+gulp.task('default', ['lint', 'build']);
+gulp.task('watch', ['setProduction', 'setWatch', 'lint', 'build']);
+gulp.task('release', ['setProduction', 'lint', 'build']);
+gulp.task('bump:major', function () {
+    bumpFn('major');
+});
+gulp.task('bump:minor', function () {
+    bumpFn('minor');
+});
+gulp.task('bump:patch', function () {
+    bumpFn('patch');
+});
---
+++ src/index.coffee
@@ -0,0 +1,4 @@
+Matrix =
+    version: '0.0.1'
+
+global.Matrix = Matrix
