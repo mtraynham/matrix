b34a39a143d5bd5471158849066f775adce5909e
Skitch <mrt6467@alum.uncw.edu>
2014-11-10 00:58:12 -0500

    Updates to index.js

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
 import {NDArray, NilNDArray, ZeroNDArray}  from './lib/NDArray';
 
-let x = new NDArray([], [], [], 0),
-    y = new NilNDArray([]),
-    z = new ZeroNDArray([], 0),
-    f = 0;
-for (var a in x) {
-    f = x[a];
-}
-for (var b in y) {
-    f = y[b];
-}
-for (var c in z) {
-    f = c[z];
-}
-
+global.NilNDArray = NilNDArray;
 global.NDArray = NDArray;
+global.ZeroNDArray = ZeroNDArray;
